261fe9c35789381e6d2d6fa133709674f24c910e
mqurban <mianqurban525@gmail.com>
2024-03-04 22:36:54 +0500

    testing

---
+++ js files/index 0.js
@@ -0,0 +1,20 @@
+function generateTable() {
+    let number = document.getElementById('table').value;
+    let limit = document.getElementById('limit').value;
+    let tableHTML = `<table><tr><th> Number </th><th> Result </th></tr>`;
+  
+    if (number < 20) {
+      for (var i = limit; i >= 1; i--) {
+        tableHTML += "<tr><td>" + number + " x " + i + "</td><td>" + (number * i) + "</td></tr>";
+      }
+    } else {
+      for (var i = 1; i <= limit; i++) {
+        tableHTML += "<tr><td>" + number + " x " + i + "</td><td>" + (number * i) + "</td></tr>";
+      }
+    }
+  
+    tableHTML += "</table>";
+    document.querySelector('.printTable').innerHTML = tableHTML;
+  }
+  
+  generateTable();
